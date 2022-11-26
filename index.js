3ec418e2722580030c9d403d1c1cdd7b31badcac
Ryan Horn <rj.horn@gmail.com>
2022-11-25 21:49:41 -0700

    commit tree 05d79954e9bf13be33ef9279f503b0932e1f507d

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity = `go to the office`){
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(outerPara, part2){
+
+        return console.log(`You are ${outerPara} ${part2} ${outerPara}`)
+        
+    
+}
+
+
